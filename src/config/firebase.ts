bd48d7061fb01177d9d47982c139ab22d856ab6a
BomberKing <bombingmap@gmail.com>
2025-11-30 22:12:29 +0100

    Hide public keys

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,13 +5,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDeLj0KksVcCGvXjWs-mXn7Dyr8r3Y6gFw",
-  authDomain: "bombingmap.firebaseapp.com",
-  projectId: "bombingmap",
-  storageBucket: "bombingmap.firebasestorage.app",
-  messagingSenderId: "868119040139",
-  appId: "1:868119040139:web:5a51e18344df5638a9848e",
-  measurementId: "G-Q0CJT2PE5H"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
